43ab01d823db2ab81bd80b213342a403b9788684
Anthony <asecreti@maryhurst.org>
2024-09-15 18:01:17 -0400

    assignment

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,2 @@
 import pandas as pd
-fruits = pd.DataFrame([[35, 21], [41, 34]], columns = ['Apples', 'Bananas'], index=['2017 Sales', '2018 Sales'])
-
-pd.read_csv(fruits)
+pd.DataFrame([[35, 21], [41, 34]], columns = ['Apples', 'Bananas'], index=['2017 Sales', '2018 Sales'])
